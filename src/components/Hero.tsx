b9b94ec9a16f2b06838519f599ddd57a7abee9cd
Lex Benjamin <abessilfie@st.ug.edu.gh>
2023-06-28 05:18:57 -0400

    Adding a Client Component -> Button to the Hero Page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,22 @@
-import Image from 'next/image'
+import Image from "next/image";
+import  CustomButton  from "./CustomButton";
 const Hero = () => {
-    return (
-        <div>Hero Section</div>
-    )
-}
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book or rent a car -- quickly and easily!
+        </h1>
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking
+          process
+        </p>
 
-export default Hero
+        {/* Custom Button */}
+        <CustomButton />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
